f9108ec475207629efe62aad67c32ecdcff74e2e
Oleh Omelchenko <ptrvtch@gmail.com>
2019-07-04 11:46:42 +0300

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gaapi4py',
+    version='0.1',
+    description='Google Analytics Reporting API v4 for Python 3',
+    url='http://github.com/ptrvtch/gaapi4py',
+    author='Oleh Omelchenko',
+    author_email='ptrvtch@gmail.com',
+    license='MIT',
+    pagkages=['gaapi4py'],
+    zip_safe=False
+)
