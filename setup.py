0a00885414d2ec991db0cdb3e0da72e9e7c153f3
Oleh Omelchenko <ptrvtch@gmail.com>
2019-07-31 10:35:27 +0000

    black formatting

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='gaapi4py',
-    version='0.1',
-    description='Google Analytics Reporting API v4 for Python 3',
-    url='http://github.com/ptrvtch/gaapi4py',
-    author='Oleh Omelchenko',
-    author_email='ptrvtch@gmail.com',
-    license='MIT',
-    pagkages=['gaapi4py'],
+    name="gaapi4py",
+    version="0.1",
+    description="Google Analytics Reporting API v4 for Python 3",
+    url="http://github.com/ptrvtch/gaapi4py",
+    author="Oleh Omelchenko",
+    author_email="ptrvtch@gmail.com",
+    license="MIT",
+    pagkages=["gaapi4py"],
     zip_safe=False,
     install_requires=[
-        'pandas',
-        'pandas-gbq',
-        'oauth2client',
-        'google-api-python-client'
+        "pandas",
+        "pandas-gbq",
+        "oauth2client",
+        "google-api-python-client",
     ],
 )
