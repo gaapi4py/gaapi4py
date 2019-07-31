bf67e33266dbe4e5b173975f9df5324135d0683f
Oleh Omelchenko <ptrvtch@gmail.com>
2019-07-31 10:20:56 +0000

    formatting according to black

--- gaapi4py/client.py
+++ gaapi4py/client.py
@@ -6,10 +6,10 @@ from oauth2client import client
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 
-SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
-API_NAME = 'analyticsreporting'
-API_VERSION = 'v4'
-USER_AGENT = 'gapy/0.1'
+SCOPES = ["https://www.googleapis.com/auth/analytics.readonly"]
+API_NAME = "analyticsreporting"
+API_VERSION = "v4"
+USER_AGENT = "gapy/0.1"
 
 
 class GAClient:
@@ -22,7 +22,8 @@ class GAClient:
         Read service key file and initialize the API client
         """
         self.credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            json_keyfile, scopes=SCOPES)
+            json_keyfile, scopes=SCOPES
+        )
         self.client = build(API_NAME, API_VERSION, credentials=self.credentials)
         self.set_view_id(None)
         self.set_dateranges(None, None)
@@ -33,43 +34,36 @@ class GAClient:
         proper request body for batch request
         """
 
-        start_date = params.get('start_date', self.start_date)
-        end_date = params.get('end_date', self.end_date)
-        include_empty_rows = params.get('include_empty_rows', True)
+        start_date = params.get("start_date", self.start_date)
+        end_date = params.get("end_date", self.end_date)
+        include_empty_rows = params.get("include_empty_rows", True)
 
         if isinstance(start_date, date):
-            start_date = datetime.strftime(start_date, '%Y-%m-%d')
+            start_date = datetime.strftime(start_date, "%Y-%m-%d")
 
         if isinstance(end_date, date):
-            end_date = datetime.strftime(end_date, '%Y-%m-%d')
+            end_date = datetime.strftime(end_date, "%Y-%m-%d")
 
-        dimensions = list(map(lambda x: {'name': x}, params.get('dimensions')))
+        dimensions = list(map(lambda x: {"name": x}, params.get("dimensions")))
 
-        metrics = list(map(lambda x: {'expression': x}, params.get('metrics')))
+        metrics = list(map(lambda x: {"expression": x}, params.get("metrics")))
 
         segments = []
-        if params.get('segments'):
-            dimensions.append({
-                'name': 'ga:segments'
-            })
-            segments.append({
-                'segmentId': params.get('segments')
-            })
+        if params.get("segments"):
+            dimensions.append({"name": "ga:segments"})
+            segments.append({"segmentId": params.get("segments")})
 
         request_body = {
-            'viewId': params.get('view_id', self.view_id),
-            'dateRanges': {
-                'startDate': start_date,
-                'endDate': end_date
-            },
-            'dimensions': dimensions,
-            'metrics': metrics,
-            'filtersExpression': params.get('filter', None),
-            'pageToken': params.get('pageToken', None),
-            'samplingLevel': 'LARGE',
-            'includeEmptyRows': include_empty_rows,
-            'segments': segments,
-            'pageSize': '10000'
+            "viewId": params.get("view_id", self.view_id),
+            "dateRanges": {"startDate": start_date, "endDate": end_date},
+            "dimensions": dimensions,
+            "metrics": metrics,
+            "filtersExpression": params.get("filter", None),
+            "pageToken": params.get("pageToken", None),
+            "samplingLevel": "LARGE",
+            "includeEmptyRows": include_empty_rows,
+            "segments": segments,
+            "pageSize": "10000",
         }
 
         return request_body
@@ -79,52 +73,51 @@ class GAClient:
         Parses and prints the Analytics Reporting API V4 response
         """
 
-        report = response_obj.get('reports', None)[0]
-        report_data = report.get('data', {})
+        report = response_obj.get("reports", None)[0]
+        report_data = report.get("data", {})
 
         result = {}
         data = []
         header_row = []
 
-        column_header = report.get('columnHeader', {})
-        metric_header = column_header.get(
-            'metricHeader', {}).get('metricHeaderEntries', [])
-        dimension_header = column_header.get('dimensions', [])
+        column_header = report.get("columnHeader", {})
+        metric_header = column_header.get("metricHeader", {}).get(
+            "metricHeaderEntries", []
+        )
+        dimension_header = column_header.get("dimensions", [])
 
         for dheader in dimension_header:
             header_row.append(dheader)
         for mheader in metric_header:
-            header_row.append(mheader['name'])
+            header_row.append(mheader["name"])
 
-        rows = report_data.get('rows', [])
+        rows = report_data.get("rows", [])
         for row in rows:
             row_temp = []
-            dimensions = row.get('dimensions', [])
-            metrics = row.get('metrics', [])
+            dimensions = row.get("dimensions", [])
+            metrics = row.get("metrics", [])
             for d in dimensions:
                 row_temp.append(d)
-            for m in metrics[0]['values']:
+            for m in metrics[0]["values"]:
                 row_temp.append(m)
             data.append(row_temp)
 
         result_df = pd.DataFrame(data, columns=header_row)
-        result_df.columns = result_df.columns.str.replace('ga:', '')
+        result_df.columns = result_df.columns.str.replace("ga:", "")
 
-        if not report_data.get('samplesReadCounts'):
+        if not report_data.get("samplesReadCounts"):
             samples_read_counts = None
             sampling_space_sizes = None
         else:
-            samples_read_counts = int(
-                report_data.get('samplesReadCounts', [])[0])
-            sampling_space_sizes = int(
-                report_data.get('samplingSpaceSizes', [])[0])
-        result['info'] = {
-            'isDataGolden': report['data'].get('isDataGolden', False),
-            'nextPageToken': report.get('nextPageToken'),
-            'samplesReadCounts': samples_read_counts,
-            'samplingSpaceSizes': sampling_space_sizes
+            samples_read_counts = int(report_data.get("samplesReadCounts", [])[0])
+            sampling_space_sizes = int(report_data.get("samplingSpaceSizes", [])[0])
+        result["info"] = {
+            "isDataGolden": report["data"].get("isDataGolden", False),
+            "nextPageToken": report.get("nextPageToken"),
+            "samplesReadCounts": samples_read_counts,
+            "samplingSpaceSizes": sampling_space_sizes,
         }
-        result['data'] = result_df
+        result["data"] = result_df
 
         return result
 
@@ -136,17 +129,18 @@ class GAClient:
         response = {}
         while True:
             request_body = self._generate_request_body(params)
-            raw_response = self.client.reports().batchGet(body={
-                'reportRequests': request_body
-            }).execute()
+            raw_response = (
+                self.client.reports()
+                .batchGet(body={"reportRequests": request_body})
+                .execute()
+            )
             parsed = self._parse_response(raw_response)
-            all_data.append(parsed['data'])
-            params['pageToken'] = parsed.get(
-                'info', {}).get('nextPageToken', None)
-            if not params['pageToken']:
-                response['info'] = parsed.get('info')
+            all_data.append(parsed["data"])
+            params["pageToken"] = parsed.get("info", {}).get("nextPageToken", None)
+            if not params["pageToken"]:
+                response["info"] = parsed.get("info")
                 break
-        response['data'] = pd.concat(all_data).reset_index(drop=True)
+        response["data"] = pd.concat(all_data).reset_index(drop=True)
         return response
 
     def set_view_id(self, view_id):
@@ -164,11 +158,11 @@ class GAClient:
             end_date: string in format 'YYYY-MM-DD'
         """
         if isinstance(start_date, str):
-            self.start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+            self.start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
         else:
             self.start_date = start_date
         if isinstance(end_date, str):
-            self.end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+            self.end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
         else:
             self.end_date = end_date
 
