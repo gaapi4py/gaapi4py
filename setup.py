e486591a5f45753914523ccb8dfcfac95c3dfe3d
Oleh Omelchenko <ptrvtch@gmail.com>
2019-08-03 11:43:33 +0000

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="gaapi4py",
-    version="1.0.0",
+    version="1.0.1",
     description="Google Analytics Reporting API v4 for Python 3",
     long_description=long_description,
     long_description_content_type="text/markdown",
